4210a04218e4e16e9941bffeb75041ce055130fc
josephcjernigan <josephc.jernigan@gmail.com>
2024-05-20 20:38:46 -0400

    Update Fraction.java

--- Gradle/JavaGradle/src/main/java/Fraction.java
+++ Gradle/JavaGradle/src/main/java/Fraction.java
@@ -1,60 +1,52 @@
 import java.io.*;
-/**
- * Purpose: demonstrate simple Java Fraction class with command line,
- * jdb debugging, and Ant build file.
- *
- * Ser321 Foundations of Distributed Applications
- * see http://pooh.poly.asu.edu/Ser321
- * @author Tim Lindquist Tim.Lindquist@asu.edu
- *         Software Engineering, CIDSE, IAFSE, ASU Poly
- * @version January 2020
- */
+
 public class Fraction {
 
-   private int numerator, denominator;
+  private int numerator, denominator;
 
-   public Fraction(){
-      numerator = denominator = 0;
-   }
+  public Fraction() {
+    numerator = denominator = 0;
+  }
 
-   public void print() {
-    System.out.print(numerator + "/" + denominator );
-   }
+  public void print() {
+    System.out.print(numerator + "/" + denominator);
+  }
 
-   public void setNumerator (int n ){
-      numerator = n;
-   }
+  public void setNumerator(int n) {
+    numerator = n;
+  }
 
-   public void setDenominator (int d) {
-      denominator = d;
-   }
+  public void setDenominator(int d) {
+    denominator = d;
+  }
 
-   public int getDenominator() {
-      return denominator;
-   }
+  public int getDenominator() {
+    return denominator;
+  }
 
-   public int getNumerator() {
-      return numerator;
-   }
+  public int getNumerator() {
+    return numerator;
+  }
 
-   public static void main (String args[]) {
-      try {
-         // create a new instance
-         // Fraction *frac = [[Fraction alloc] init];
-         Fraction frac = new Fraction();
+  public static void main(String args[]) {
+    try {
+      // create a new instance
+      Fraction frac = new Fraction();
 
-         // set the values
-         frac.setNumerator(1);
-         frac.setDenominator(3);
+      // set the values with arguments or defaults
+      int numerator = args.length > 0 ? Integer.parseInt(args[0]) : 1;
+      int denominator = args.length > 1 ? Integer.parseInt(args[1]) : 3;
 
-         // print it
-         System.out.print("The fraction is: ");
-         frac.print();
-         System.out.println("");
+      frac.setNumerator(numerator);
+      frac.setDenominator(denominator);
 
-      }catch(Exception e) {
-         e.printStackTrace();
-      }
-   }
+      // print it
+      System.out.print("The fraction is: ");
+      frac.print();
+      System.out.println("");
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
-
